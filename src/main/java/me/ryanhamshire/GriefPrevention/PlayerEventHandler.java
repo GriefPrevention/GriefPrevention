80898c512b3eef443f3e92431e42d27366cbe7df
Noxillio <me@noxillio.dev>
2025-03-12 00:26:53 -0400

    Revise the ROOTED_DIRT dupe fix

--- src/main/java/me/ryanhamshire/GriefPrevention/PlayerEventHandler.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/PlayerEventHandler.java
@@ -1564,7 +1564,7 @@ class PlayerEventHandler implements Listener
                                 clickedBlockType == Material.LOOM ||
                                 clickedBlockType == Material.PUMPKIN ||
                                 clickedBlockType == Material.RESPAWN_ANCHOR ||
-                                clickedBlockType == Material.ROOTED_DIRT ||
+                                (clickedBlockType == Material.ROOTED_DIRT && player.getInventory().getItemInMainHand().getType().toString().toLowerCase().endsWith("_hoe") && player.getInventory().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH)) ||
                                 clickedBlockType == Material.STONECUTTER ||
                                 clickedBlockType == Material.SWEET_BERRY_BUSH ||
                                 clickedBlockType == Material.DECORATED_POT
