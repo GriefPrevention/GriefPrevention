ed57669a72a349156a82d0fcad64e86f09cf2a8a
FreeMonoid <49086127+FreeMonoid@users.noreply.github.com>
2025-12-02 22:34:25 +0300

    Make luredByPlayer key completely static

--- src/main/java/me/ryanhamshire/GriefPrevention/EntityDamageHandler.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/EntityDamageHandler.java
@@ -73,19 +73,15 @@ public class EntityDamageHandler implements Listener
             EntityType.PANDA
     );
 
+    private static final NamespacedKey LURED_BY_PLAYER = NamespacedKey.fromString("griefprevention:lured_by_player");
+
     private final @NotNull DataStore dataStore;
     private final @NotNull GriefPrevention instance;
-    private static NamespacedKey luredByPlayer;
 
     EntityDamageHandler(@NotNull DataStore dataStore, @NotNull GriefPrevention plugin)
     {
         this.dataStore = dataStore;
         instance = plugin;
-        // Static field initialization - safe because only one instance is created at startup
-        if (luredByPlayer == null)
-        {
-            luredByPlayer = new NamespacedKey(plugin, "lured_by_player");
-        }
     }
 
     // Tag passive animals that can become aggressive so that we can tell whether they are hostile later
@@ -99,9 +95,9 @@ public class EntityDamageHandler implements Listener
             return;
 
         if (event.getReason() == EntityTargetEvent.TargetReason.TEMPT)
-            event.getEntity().getPersistentDataContainer().set(luredByPlayer, PersistentDataType.BYTE, (byte) 1);
+            event.getEntity().getPersistentDataContainer().set(LURED_BY_PLAYER, PersistentDataType.BYTE, (byte) 1);
         else
-            event.getEntity().getPersistentDataContainer().remove(luredByPlayer);
+            event.getEntity().getPersistentDataContainer().remove(LURED_BY_PLAYER);
 
     }
 
@@ -301,7 +297,7 @@ public class EntityDamageHandler implements Listener
             return rabbit.getRabbitType() == Rabbit.Type.THE_KILLER_BUNNY;
 
         if ((TEMPTABLE_SEMI_HOSTILES.contains(type)) && entity instanceof Mob mob)
-            return !entity.getPersistentDataContainer().has(luredByPlayer, PersistentDataType.BYTE) && mob.getTarget() != null;
+            return !entity.getPersistentDataContainer().has(LURED_BY_PLAYER, PersistentDataType.BYTE) && mob.getTarget() != null;
 
         return false;
     }
